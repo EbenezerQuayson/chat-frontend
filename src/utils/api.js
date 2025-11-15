ddf40b0211ee239c56990f851fc76699c3d92d06
Ebenezer Quayson <2425401402@gctu.live.edu>
2025-11-15 02:18:51 +0000

    Changed the api link

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set your backend base URL (adjust if backend runs elsewhere)
 const API = axios.create({
-  baseURL: "http://localhost/chatapp/php/",
+  baseURL: "https://chatapp-backend.infinityfree.me/chatapp/api",
   withCredentials: true, // allows sessions/cookies if used
 });
 
